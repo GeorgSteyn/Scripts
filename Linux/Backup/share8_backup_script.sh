ea8d3123beb676aef02f7d8993e25efdbd93a21a
Georg Steyn <georgsteyn@gmail.com>
2016-11-21 11:11:57 +0200

    Made the Share8 Backup Script for Work
    Read The Commnets always

---
+++ Linux/Backup/share8_backup_script.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# Mount the drive to the correct path. DO THIS MANUALLY ALWAYS!!!
+cp -r -u -v 1475624130_water.ico \
+Andries/ \
+_backup/ \
+Downloads.7z \
+gerrie/ \
+gxPublisher.exe \
+klientedata/ \
+Marnus/ \
+Norman/ \
+Pret/ \
+1475624202_bolt.ico \
+argiewe/ \
+Bart/ \
+Fransua/ \
+gert/ \
+ICD-10/ \
+MantisTimer.exe \
+NHN/ \
+programmers/ \
+Alan/ \
+_argiewe/ \
+CHF/ \
+Georg/ \
+gx32test/ \
+Installs/ \
+Margrit/ \
+nicholas/ \
+OVF_Virtual_Machine_Templates_dont_delete_please/ \
+Pryse/ \
+Bernhardt/ \
+-t /mnt/sdd1/share8/
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Scripts
+My Scripts for Linux & Windows
