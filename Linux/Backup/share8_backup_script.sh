fcf6e9324b784e8eeb61b1f6d3632a9cfe374549
Georg Steyn <GeorgSteyn@users.noreply.github.com>
2017-02-17 15:28:40 +0200

    Updated Script
    Update Script

--- Linux/Backup/share8_backup_script.sh
+++ Linux/Backup/share8_backup_script.sh
@@ -1,34 +1,3 @@
 #!/bin/sh
 # Mount the drive to the correct path. DO THIS MANUALLY ALWAYS!!!
-cp -r -u -v 1475624130_water.ico \
-argiewe/ \
-Bernhardt/ \
-Georg/ \
-gxPublisher.exe \
-MantisTimer.exe \
-nicholas/ \
-Pret/ \
-1475624202_bolt.ico \
-_argiewe/ \
-CHF/ \
-gerrie/ \
-ICD-10/ \
-Margrit/ \
-Norman/ \
-programmers/ \
-Alan/ \
-_backup/ \
-Downloads.7z \
-gert/ \
-Installs/ \
-Marnus/ \
-Pryse/ \
-Andries/ \
-Bart/ \
-Fransua/ \
-gx32test/ \
-klientedata/ \
-NHN/ \
-OVF_Virtual_Machine_Templates_dont_delete_please/ \
-upsource-3.5.3597.zip* \
--t /mnt/sdd1/share8/
+cp -r -u -v 1475624130_water.ico 1475624130_water.ico Alan/ _argiewe/ Bernhardt/ Fransua/ gerrie/ ICD-10/ MantisTimer.exe NHN/ programmers/ 1475624202_bolt.ico Andries/ _backup/ CHF/ garth/ gert/ Installs/ Margrit/ nicholas/ Pryse/ 2017_Tariewe/ argiewe/ Bart/ Downloads.7z Georg/ gx32test/ klientedata/ Marnus/ Norman/ Pret/ upsource-3.5.3597.zip -t /mnt/sdd1/share8/
