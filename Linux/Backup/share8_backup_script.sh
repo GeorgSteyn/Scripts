38a98d1b0845096a41a0f9468fc48b481668ae95
Georg Steyn <georgsteyn@gmail.com>
2018-07-16 11:52:10 +0200

    Delete share8_backup_script.sh

--- Linux/Backup/share8_backup_script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# Mount the drive to the correct path. DO THIS MANUALLY ALWAYS!!!
-cp -r -u -v 0023.pdf Andries/ Bernhardt/ gd6_20170220gd6_bdrive.rar ICD-10/ Marnus/ SQLAccrossRegions.exe 1475624130_water.ico argiewe/ CHF/ Georg/ Installs/ NHN/ Pret/ upsource-3.5.3597.zip* 1475624202_bolt.ico _argiewe/ Downloads.7z gerrie/ klientedata/ nicholas/ programmers/ 2017_Tariewe/ _backup/ Fransua/ gert/ MantisTimer.exe Norman/ Pryse/ Alan/ Bart/ garth/ gx32test/ Margrit/ run_mantistimer.bat -t /mnt/sdd1/share8/
